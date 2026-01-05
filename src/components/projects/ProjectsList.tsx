65bd938bded2b7abf291e4db8df589dc2d013441
Sandaruwan2000 <it22563200@my.sliit.lk>
2026-01-05 20:57:23 +0530

    fixed backgroun color

--- src/components/projects/ProjectsList.tsx
+++ src/components/projects/ProjectsList.tsx
@@ -310,7 +310,7 @@ export default function ProjectsList({ onCreateNew, onEdit, refreshTrigger }: Pr
       {showBacklogModal && selectedProject && (
         <div className="fixed inset-0 z-50 overflow-y-auto">
           <div
-            className="fixed inset-0 bg-black bg-opacity-50 transition-opacity"
+            className="fixed inset-0 bg-black/60 transition-opacity"
             onClick={() => {
               setShowBacklogModal(false);
               setSelectedProject(null);
